fdc7ee59225d7203e8948567ce494e89a75a0104
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-06-26 18:20:54 -0400

    feat: Se agrega una tabla a listado_producto.html para listar productos y validar la consulta ORM

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def lista_productos(request):
-    return render(request, 'productos/lista_productos.html')
+    productos = Producto.objects.all()
+    context = { 'productos': productos, }
+    return render(request, 'productos/lista_productos.html', context)
