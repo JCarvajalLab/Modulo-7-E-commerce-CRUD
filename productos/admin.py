4c7ccc67cf1a1877424e202ed27dbc9f5e1eae58
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-06-26 17:05:50 -0400

    feat: se registra el modelo Producto y también se agrega en Django Admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from.models import Categoria
+from.models import Categoria, Producto
 
-admin.site.register(Categoria)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('nombre',)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+        'categoria',
+        'precio',
+        'stock',
+        'creado_en',)
+    search_fields = (
+        'nombre',
+        'descripcion',
+    )
+    list_filter = (
+        "categoria",
+    )
+    ordering = (
+        "nombre",
+    )
--- productos/models.py
+++ productos/models.py
@@ -9,4 +9,26 @@ class Categoria(models.Model):
     class Meta:
         verbose_name = "Categoria"
         verbose_name_plural = "Categorias"
-        ordering = ['id']
+        ordering = ['nombre']
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name="Nombre")
+    descripcion = models.TextField(verbose_name="Descripcion")
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    stock = models.PositiveIntegerField(default=0, verbose_name="Stock")
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.PROTECT,
+        related_name="productos",
+        verbose_name="Categoria"
+    )
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name="Producto"
+        verbose_name_plural = "Productos"
+        ordering = ['nombre']
