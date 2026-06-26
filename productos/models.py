46ad751beaf9d839da542252541dc71c24337a03
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-06-26 14:54:34 -0400

    feat: Se crea y se registrar modelo Categoria

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True, verbose_name="Nombre")
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ['id']
