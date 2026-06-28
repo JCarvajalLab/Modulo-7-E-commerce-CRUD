fb90facb43ea81f33d96deed458e4d13645daa2b
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-06-28 14:38:30 -0400

    feat: se agrega la creación de productos(funcionalidad al presionar guardar)

--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Producto
 from .forms import ProductoForms
 
@@ -10,6 +10,12 @@ def lista_productos(request):
     return render(request, 'productos/lista_productos.html', context)
 
 def crear_producto(request):
-    form = ProductoForms()
+    if request.method == "POST":
+        form = ProductoForms(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForms()
     context = {'form': form}
     return render(request, 'productos/crear_producto.html', context)
